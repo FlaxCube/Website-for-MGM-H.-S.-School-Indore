491cdd2d417a6accb86ea24e9b26592c82f8a506
FlaxCube <81681912+FlaxCube@users.noreply.github.com>
2024-01-30 12:02:32 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,24 @@
-
+// Read more for Streams section 
 document.addEventListener('DOMContentLoaded', function() {
     var readMoreButtons = document.querySelectorAll('.read-more');
 
     readMoreButtons.forEach(function(button) {
         button.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent the default behavior of the link
-
+            event.preventDefault();
             var additionalContent = this.previousElementSibling;
 
             if (additionalContent.style.display === 'none') {
                 additionalContent.style.display = 'block';
-                this.textContent = 'Read Less'; // Update the button text
+                this.textContent = 'Read Less';
             } else {
                 additionalContent.style.display = 'none';
-                this.textContent = 'Read More'; // Update the button text
+                this.textContent = 'Read More';
             }
         });
     });
 });
 
+// Contact form Error if user does not fill out all fields
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById("contact-form");
 
@@ -32,5 +32,31 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("Please fill in all fields.");
             event.preventDefault();
         }
+    });
+});
+
+// Arrow to go back to the top of the web
+document.addEventListener("DOMContentLoaded", function() {
+    var backToTop = document.getElementById("back-to-top");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 100) {
+            backToTop.style.display = "block";
+        } else {
+            backToTop.style.display = "none";
+        }
+    });
+
+    backToTop.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var loadingOverlay = document.getElementById("loading-overlay");
+    window.addEventListener("load", function() {
+        loadingOverlay.style.display = "none";
     });
 });
